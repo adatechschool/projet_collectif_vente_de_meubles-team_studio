b5371c281d4cd8966b124dd4770599c98c5a2c73
Alice <102388803+Alicexplore@users.noreply.github.com>
2023-03-14 15:34:04 +0100

    adding routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import {Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
+import {Home} from './pages/Home';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Banner />
+      <a href="/home">click maison</a>
+      <>
+      <Routes>
+        <Route path='/home' element={<Home/>}/>
+      </Routes>
+      </> 
     </div>
   );
 }
