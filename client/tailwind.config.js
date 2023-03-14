bcfd4c132735f94ea1639b1a96ccaad18b56a354
Alice <102388803+Alicexplore@users.noreply.github.com>
2023-03-14 14:15:56 +0100

    build Banner

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        worksans: ['Work Sans', 'sans-serif'],
+        apercu: ['Apercu Pro', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
