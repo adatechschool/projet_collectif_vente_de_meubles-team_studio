40458c5e35952b1a561f80503538151aefa67abe
HortenseLaclaviere <114992888+HortenseLaclaviere@users.noreply.github.com>
2023-03-14 15:39:37 +0100

    Build Cards in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import DisplayCards from './components/DisplayCards'
 
 function App() {
   return (
-    <div className="App">
-      Salut la team
+    <div>
+    <DisplayCards/>
     </div>
   );
 }
---
+++ client/src/components/DisplayCards.js
@@ -0,0 +1,20 @@
+export default function DisplayCards(){
+    return(
+        <section>
+            <div className="grid grid-cols-3 gap-4  h-screen items-center justify-center">
+                
+                <div className="border-2 bg-gray-200 w-24 h-24"></div>
+                <div className="border-2 bg-gray-200 w-24 h-24"></div>
+                <div className="border-2 bg-gray-200 w-24 h-24"></div>
+                <div className="border-2 bg-gray-200 w-24 h-24"></div>
+                <div className="border-2 bg-gray-200 w-24 h-24"></div>
+                <div className="border-2 bg-gray-200 w-24 h-24"></div>
+                <div className="border-2 bg-gray-200 w-24 h-24"></div>
+                <div className="border-2 bg-gray-200 w-24 h-24"></div>
+                <div className="border-2 bg-gray-200 w-24 h-24"></div>
+                
+
+            </div>
+        </section>
+    )
+}
