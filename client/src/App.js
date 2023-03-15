00f8297ca59ddc3b0bdebd3acac3571ab95bf3da
HortenseLaclaviere <114992888+HortenseLaclaviere@users.noreply.github.com>
2023-03-15 14:43:33 +0100

    Changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
-import DisplayCards from './components/DisplayCards'
+import DisplayCards from './components/DisplayCards';
 import {Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
 import {Home} from './pages/Home';
-import DisplayCards from './components/DisplayCards';
 
 function App() {
   return (
     <div>
-    <DisplayCards/>
       <Navbar/>
       <Banner />
       {/* <a href="/home">click maison</a> */}
