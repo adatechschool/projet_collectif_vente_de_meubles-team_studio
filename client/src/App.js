fa5bb764d04e51ba71bd44a280732c6469b9034d
HortenseLaclaviere <114992888+HortenseLaclaviere@users.noreply.github.com>
2023-03-15 10:46:36 +0100

    Build cards

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,20 @@ import {Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
 import {Home} from './pages/Home';
+import DisplayCards from './components/DisplayCards';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Banner />
-      <a href="/home">click maison</a>
+      {/* <a href="/home">click maison</a> */}
       <>
       <Routes>
-        <Route path='/home' element={<Home/>}/>
+        {/* <Route path='/home' element={<Home/>}/> */}
       </Routes>
       </> 
+      <DisplayCards/>
     </div>
   );
 }
