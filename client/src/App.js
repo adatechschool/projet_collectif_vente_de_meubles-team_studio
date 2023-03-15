40b1cc00b300e5f915e4804129fbc63f0aaf027d
Hortense LACLAVIERE <114992888+HortenseLaclaviere@users.noreply.github.com>
2023-03-15 12:12:04 +0100

    Update Add.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,10 @@ import {Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
 import {Home} from './pages/Home';
-import DisplayCards from './components/DisplayCards';
 
 function App() {
   return (
     <div>
-    <DisplayCards/>
       <Navbar/>
       <Banner />
       {/* <a href="/home">click maison</a> */}
