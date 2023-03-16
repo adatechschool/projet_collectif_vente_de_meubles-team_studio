fcebe84a494ea18377edeff1765d21b92f342ac5
Gabrielparizet <parizetgabriel@gmail.com>
2023-03-16 17:12:54 +0100

    (bugfix) Modify fetching port in fetching.js

--- client/src/components/Fetching.js
+++ client/src/components/Fetching.js
@@ -1,5 +1,5 @@
 export default function Fetching(){
-    fetch('http://localhost:3003/furnitures')
+    fetch('http://localhost:9000/furnitures')
         .then(response => response.json())
         .then(data => {
                 console.log(data)
