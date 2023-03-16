1e609b71f6c702f7579a92d233ffd3a4e4aceb97
amandaparaizo <amandaparaizo@gmail.com>
2023-03-16 12:00:41 +0100

    search bar finie

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -10,7 +10,6 @@ export default function Navbar() {
   const searchBar = () => {
     console.log("search bar");
   };
-  
 
   return (
     <div className="bg-[#151515] relative h-10 items-center justify-center flex z-50">
@@ -23,7 +22,7 @@ export default function Navbar() {
         <button type="button" onClick={showCart}>
           <img src="./img/cart.svg" className="h-5 w-5 " alt="Access cart" />
         </button>
-        
+
         <button type="button" onClick={userConnect}>
           <img
             src="./img/user.svg"
@@ -32,111 +31,31 @@ export default function Navbar() {
           />
         </button>
 
-        {/* Pour la barre de recherche et le dropdown menu*/}
-      </div>
-
-      <form>
-        <div class="flex">
-          <button
-            id="dropdown-button"  
-            //data-dropdown-toggle="dropdown"
-            className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:dark-gray-900 dark:bg-gray-800 dark:hover:bg-gray-700 dark:text-white"
-            type="button"
-            
-          >
-            All categories{" "}
-            <svg
-              aria-hidden="true"
-              class="w-3 h-3 ml-1"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </button>
-          <div
-            id="dropdown"
-            className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-800"
-          >
-            <ul
-              class="py-2 text-sm text-gray-700 dark:text-gray-200"
-              aria-labelledby="dropdown-button"
-            >
-              <li>
-                <button
-                  type="button"
-                  class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  Canapés
-                </button>
-              </li>
-              <li>
-                <button
-                  type="button"
-                  class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  Tables
-                </button>
-              </li>
-              <li>
-                <button
-                  type="button"
-                  class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  Chaises
-                </button>
-              </li>
-              <li>
-                <button
-                  type="button"
-                  class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                >
-                  Logos
-                </button>
-              </li>
-            </ul>
-          </div>
-
-          <div class="relative w-full">
+        <form>
+          <div class="relative">
+            <div class="h-0 w-0"></div>
             <input
               type="search"
-              id="search-dropdown"
-              class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 border-l-gray-50 border-l-2 border border-gray-300  focus:dark-black-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
-              placeholder="Search furniture..."
+              onClick={searchBar}
+              id="default-search"
+              class="block w-full p-1
+         pl-5 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-black-800 dark:bg-gray-800 dark:border-gray-700 dark:placeholder-gray-400 dark:text-white"
+              placeholder=" Search furnitures..."
               required
-              
             />
             <button
               type="submit"
-              class="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-gray-800 border-gray-800 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-800 dark:bg-gray-800 dark:hover:bg-gray-800 dark:focus:ring-gray-800"
-              onClick={searchBar}
+              class="absolute right-1 bottom-1 text-white  bg-[#151515] focus:ring-3 focus:outline-none focus:ring-[#151515] font-small rounded-lg text-sm px-3 py-3 dark:[#151515]"
             >
-              <svg
-                aria-hidden="true"
-                class="w-5 h-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                ></path>
-              </svg>
-              <span class="sr-only">Search</span>
+              <img
+                src="./img/magnifyng_glass.png"
+                className="absolute left-1 bottom-1 h-4 w-4"
+                alt="Search Bar"
+              />
             </button>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 }
-
