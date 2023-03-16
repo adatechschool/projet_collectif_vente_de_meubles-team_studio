0bd66b2919d811c5c05479f294143cc1a3c491e9
Alice <102388803+Alicexplore@users.noreply.github.com>
2023-03-16 14:16:56 +0100

    add files Alice

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,19 @@ import {Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
 import {Home} from './pages/Home';
+import {Login} from './pages/Login';
+import {Informations} from './pages/Informations'
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Banner />
-      <a href="/home">click maison</a>
       <>
       <Routes>
+        <Route path='/login' element={<Login/>}/>
         <Route path='/home' element={<Home/>}/>
+        <Route path='/informations' element={<Informations/>}/>
       </Routes>
       </> 
     </div>
