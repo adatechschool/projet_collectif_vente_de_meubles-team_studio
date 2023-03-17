89872442d1d6cb4c76fc6b8a1ecc1e8acdb8d51a
Gabrielparizet <parizetgabriel@gmail.com>
2023-03-17 14:47:44 +0100

    (build) Modify post order route to include user_id

--- api/routes/order.js
+++ api/routes/order.js
@@ -11,14 +11,15 @@ const sequelize = new Sequelize(process.env.database, process.env.user, process.
 });
 
 router.post('/', (req, res, next) => {
-    try {
-        let sql = "SELECT furniture.furniture_Id, image.image_name, furniture.furniture_type, furniture.furniture_price FROM furniture INNER JOIN image ON furniture.furniture_id = image.image_furniture_id;"
-        sequelize.query(sql)
-        .then(([result]) => {
-          result = createArrayOfImagesFromResultJsonObject(result);
-              console.log(result)
-              res.send(result)})
-    } catch (error) {  console.error('Impossible de se connecter, erreur suivante :', error); }
+    
+    // try {
+    //     let sql = "SELECT furniture.furniture_Id, image.image_name, furniture.furniture_type, furniture.furniture_price FROM furniture INNER JOIN image ON furniture.furniture_id = image.image_furniture_id;"
+    //     sequelize.query(sql)
+    //     .then(([result]) => {
+    //       result = createArrayOfImagesFromResultJsonObject(result);
+    //           console.log(result)
+    //           res.send(result)})
+    // } catch (error) {  console.error('Impossible de se connecter, erreur suivante :', error); }
     res.send('The order route is working');
 });
 
