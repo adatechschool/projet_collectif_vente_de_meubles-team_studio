7ab15ef1960435a22787817881972b0dc8d9e481
Gabrielparizet <parizetgabriel@gmail.com>
2023-03-17 17:14:52 +0100

    (build) Ongoing build of order route

--- api/routes/order.js
+++ api/routes/order.js
@@ -3,26 +3,28 @@ const { Sequelize } = require('sequelize');
 var router = express.Router();
 const app = express();
 require('dotenv').config();
+const bodyParser = require('body-parser')
 
 // Database connection credentials:
 const sequelize = new Sequelize(process.env.database, process.env.user, process.env.password, {
     dialect: 'mysql', host: 'localhost'
 });
 
+router.use(bodyParser.urlencoded({ extended: true}));
 
-// router.post('/user/:user_id/order', (req, res, next) => {
-//     const user_id = parseInt(req.params.user_id);
-//     console.log(user_id);
-//     try {
-//         let sql = `INSERT INTO 'order' (order_id, order_date, order_user_id) VALUES (NULL, NOW(), ${user_id});`;
-//         sequelize.query(sql, {
-//             type: sequelize.QueryTypes.INSERT
-//         }).then(data => {
-//             if (data) {
-//                 console.log('Order added.')
-//             }
-//         })
-//     } catch (error) { console.error('Impossible de se connecter, erreur suivante:', error); }
-// });
+router.post('/user/:user_id/order', (req, res, next) => {
+    const user_id = parseInt(req.params.user_id);
+    console.log(user_id);
+    try {
+        let sql = `INSERT INTO 'order' (order_id, order_date, order_user_id) VALUES (NULL, NOW(), ${user_id});`;
+        sequelize.query(sql, {
+            type: sequelize.QueryTypes.INSERT
+        }).then(data => {
+            if (data) {
+                console.log('Order added.')
+            }
+        })
+    } catch (error) { console.error('Impossible de se connecter, erreur suivante:', error); }
+});
 
 module.exports = router;
