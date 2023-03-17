5c2781ba9f5b21f31dc7f1b027c6ca7125ec4ccc
Gabrielparizet <parizetgabriel@gmail.com>
2023-03-17 16:30:20 +0100

    (build) Ongoing building of post order route

--- api/routes/furnitures.js
+++ api/routes/furnitures.js
@@ -3,7 +3,6 @@ const { Sequelize } = require('sequelize');
 var router = express.Router();
 const app = express();
 require('dotenv').config();
-console.log(process.env)
 
 const sequelize = new Sequelize(process.env.database, process.env.user, process.env.password, {
   dialect: 'mysql', host: 'localhost'
--- api/routes/order.js
+++ api/routes/order.js
@@ -3,24 +3,26 @@ const { Sequelize } = require('sequelize');
 var router = express.Router();
 const app = express();
 require('dotenv').config();
-console.log(process.env)
 
 // Database connection credentials:
 const sequelize = new Sequelize(process.env.database, process.env.user, process.env.password, {
     dialect: 'mysql', host: 'localhost'
 });
 
-router.post('/', (req, res, next) => {
-    
-    // try {
-    //     let sql = "SELECT furniture.furniture_Id, image.image_name, furniture.furniture_type, furniture.furniture_price FROM furniture INNER JOIN image ON furniture.furniture_id = image.image_furniture_id;"
-    //     sequelize.query(sql)
-    //     .then(([result]) => {
-    //       result = createArrayOfImagesFromResultJsonObject(result);
-    //           console.log(result)
-    //           res.send(result)})
-    // } catch (error) {  console.error('Impossible de se connecter, erreur suivante :', error); }
-    res.send('The order route is working');
-});
+
+// router.post('/user/:user_id/order', (req, res, next) => {
+//     const user_id = parseInt(req.params.user_id);
+//     console.log(user_id);
+//     try {
+//         let sql = `INSERT INTO 'order' (order_id, order_date, order_user_id) VALUES (NULL, NOW(), ${user_id});`;
+//         sequelize.query(sql, {
+//             type: sequelize.QueryTypes.INSERT
+//         }).then(data => {
+//             if (data) {
+//                 console.log('Order added.')
+//             }
+//         })
+//     } catch (error) { console.error('Impossible de se connecter, erreur suivante:', error); }
+// });
 
 module.exports = router;
