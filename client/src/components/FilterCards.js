99ca04ec4aa4ddf19ce748b95f27cf2d80d29fff
Gabrielparizet <parizetgabriel@gmail.com>
2023-03-22 17:12:42 +0100

    (feat) Add input form in Home

--- client/src/components/FilterCards.js
+++ client/src/components/FilterCards.js
@@ -1,8 +1,17 @@
 import { useState } from "react";
+import Select from 'react-select'
+
+const optionsType = [
+    { value: 'chair', label: 'Chair' },
+    { value: 'desk', label: 'Desk' },
+    { value: 'sofa', label: 'Sofa' },
+    { value: 'table', label: 'Table'}
+  ]
 
 const FilterCards = () => {
-
-
+    return (
+        <Select options={optionsType}/>
+    )
 
 }
 
